11afd0db2757b990506ee0cd0e961fa091a7ac3a
x13.me <17013579+x13-me@users.noreply.github.com>
2025-10-26 01:26:34 +0000

    add menuboot as submodule

--- flake.nix
+++ flake.nix
@@ -23,5 +23,6 @@
         tag = "6.18-rc2-S211d-Deeea-Pb94e-C5175H3e0d-HK01ba-Vc222-B14f4-R448a";
         sha256 = "sha256-H0LUFu7yFi9d6xKoHy1jw9yqmCxJL/9kz1aWNO4d0Mo=";
       };
+      default = packages.aarch64-linux.kernel;
     };
 }
